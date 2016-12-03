dc4f6db935e3da6269d5856490f507acdf3bc44e
eswaribala <Parameswaribala@gmail.com>
2016-12-03 17:28:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+﻿var insmodule = angular.module('InsModule', []);
+insmodule.controller('ClaimController',
+    ['$scope', '$http', function ($scope, $http) {
+
+        $http(
+            {
+                method: 'get',
+                datatype: 'jsonp',
+                
+                headers: {
+                    'Content-Type': 'application/json'
+
+                },
+                url: 'http://localhost:20791/Policy'
+
+            }).success(function (data) {
+                $scope.result = data;
+                console.log($scope.result);
+
+            });
+
+
+}]);
